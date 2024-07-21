ca0643368a9ba59c9584621bf92145eb0778ce35
Haroon <you@example.com>
2024-07-21 12:27:45 +0300

    add packges and connect with MDB

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-// Here is where we import modules
-// We begin by loading Express
-const express = require("express");
 
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
+const express = require("express");
+const mongoose = require("mongoose"); // require package
 const app = express();
+
+
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+    app.get("/", async (req, res) => {
+        res.render("index.ejs");
+    });
+
+    
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
