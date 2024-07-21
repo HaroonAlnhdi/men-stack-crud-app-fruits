c7b8066391f73a6af7d2a618b411418f284d92a6
Haroon <you@example.com>
2024-07-21 15:24:53 +0300

    bulid the fruits index page

--- server.js
+++ server.js
@@ -29,6 +29,11 @@
             res.render("index.ejs");
         });
 
+        // GET /fruits -  مهم طباعة البيانات من قاعدة البيانات  على الصفحة 
+        app.get("/fruits", async (req, res) => {
+            const allFruits = await Fruit.find();
+            res.render("fruits/index.ejs", { fruits: allFruits });
+          });
 
         app.get("/fruits/new",(req,res) => {
             res.render("fruits/new.ejs");
@@ -36,7 +41,7 @@
 
       
 
-        // POST /fruits
+        // POST /fruits - مهم جدا ارسال مدخلات من الصفحة الى قاعدة البيانات 
         app.post("/fruits", async (req, res) => {
             if (req.body.isReadyToEat === "on") {
               req.body.isReadyToEat = true;      //  change value from on to true in DataBase ;
@@ -44,7 +49,8 @@
               req.body.isReadyToEat = false;    //  change value from off to false in DataBase ;
             }
             await Fruit.create(req.body);      // add these input from (form) to DB..
-            res.redirect("/fruits/new");      // نستخدمة بعد ارسال البيانات عشان مايكرر العملية عدة مرات في قاعدة البيانات  -يضمن ان العملية تتنفذ مرة  بعد ضغط ارسال 
+            // res.redirect("/fruits/new");      // نستخدمة بعد ارسال البيانات عشان مايكرر العملية عدة مرات في قاعدة البيانات  -يضمن ان العملية تتنفذ مرة  بعد ضغط ارسال 
+            res.redirect("/fruits"); // redirect to index fruits
           });
 
         app.listen(3000, () => {
